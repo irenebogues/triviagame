7b06b499ae7e70c937e7a67d24bed7c51dc6c2d8
izzyme <irenebogues@gmail.com>
2018-10-26 11:48:26 -0400

    game timer starts when documents loads

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,45 @@
 // from https://trivia.fyi/
 
-var triviaGame = {
-    // Create an array of objects, each one representing a question
-    questions: [ { q: "Which of the following is not a member of OPEC?", a1: "United States", a2: "Venezuela", a3: "Singapore", a4: "Saudi Arabia", correct: 3, comment: "Singapore is not a member of OPEC"},
-                 { q: "What is the only mammal born with horns?", a1: "Elephants", a2: "Rhinceros", a3: "Giraffe", a4: "Wild Boar", correct: 3, comment: "Giraffe is the only mammal with horns"},
-                 { q: "In what city you would find the Wizard of Oz?", a1:"Kansas City", a2: "Emerald City", a3: "Lion City", a4: "The City of Gold", correct: 2, comment: "The correct answer is Emerald City"},
-                 { q: "?", a1: "", a2: "", a3: "", a4: "", correct: 0, comment: "" } ],
-    winCount: 0,
-    loseCount: 0,
- };
 
-function triggerQuestions () {
-    $("#trivia").empty();
+
+
+
+
+// 
+
+$(document).ready(function() {
+    var timeleft = 60;
+    var downloadTimer = setInterval(function(){
+    timeleft--;
+     document.getElementById('timer').textContent = timeleft;
+    
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    },1000)
+  });
+
+
+
+
+
+  
+
+
+
+
+
+// var triviaGame = {
+//     Create an array of objects, each one representing a question
+//     questions: [ { q: "Which of the following is not a member of OPEC?", a1: "United States", a2: "Venezuela", a3: "Singapore", a4: "Saudi Arabia", correct: 3, comment: "Singapore is not a member of OPEC"},
+//                  { q: "What is the only mammal born with horns?", a1: "Elephants", a2: "Rhinceros", a3: "Giraffe", a4: "Wild Boar", correct: 3, comment: "Giraffe is the only mammal with horns"},
+//                  { q: "In what city you would find the Wizard of Oz?", a1:"Kansas City", a2: "Emerald City", a3: "Lion City", a4: "The City of Gold", correct: 2, comment: "The correct answer is Emerald City"},
+//                  { q: "?", a1: "", a2: "", a3: "", a4: "", correct: 0, comment: "" } ],
+//     winCount: 0,
+//     loseCount: 0,
+//  };
+
+// function triggerQuestions () {
+//     $("#trivia").empty();
  
 
 
@@ -28,14 +56,21 @@ function triggerQuestions () {
 
        //Generating the options with a loop
        //Create a for loop for each array
-       i = 0;
-       for (var i = 1; i < triviaGame.questions.length; i++) { // .length to go thru the questions each
-           //
-        $("#quizzes").append('<input id="quiz' + i + '" type="radio">' + " " + currentObject['a'+i] + '</input><br>'); 
-        //create a variable that reference the question
-        //
-    }
-}
+//        i = 0;
+//        for (var i = 1; i < triviaGame.questions.length; i++) { // .length to go thru the questions each
+//            //
+//         $("#quizzes").append('<input id="quiz' + i + '" type="radio">' + " " + currentObject['a'+i] + '</input><br>'); 
+//         //create a variable that reference the question
+//         //
+//     }
+// }
+
+
+
+
+
+
+
 
 //capture the user inputs
 //check the right and wrong
